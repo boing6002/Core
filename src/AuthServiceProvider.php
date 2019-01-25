2a92efe0beddb8b97dc7e013932b675942538b73
boing6002 <45848144+boing6002@users.noreply.github.com>
2019-01-25 16:24:01 -0200

    Update AuthServiceProvider.php

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -17,8 +17,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Horizon::auth(function ($request) {
+        /*Horizon::auth(function ($request) {
             return auth()->check() && $request->user()->isAdmin();
-        });
+        });*/
     }
 }
